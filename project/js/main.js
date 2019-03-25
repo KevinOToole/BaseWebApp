82a6c5984b4920b0cdbb104f96f9a1adf171bed7
X00149930 <x00149930@COMPUTING.STU.IT-TALLAGHT.IE>
2019-03-25 11:39:55 +0000

    Added login button

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,25 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+  $("#demo").html("wubba lubba dub dub");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/snuffles-was-my-slave-name.jpg" />`);
+  $("#snuffles-image").append(`<img class="img-square" src="images/snuffles-was-my-slave-name.jpg" />`);
 }
 
+// Gets called whenever the user clicks "sign in" or "sign out".
+function toggleLoginFunction() {
+  if (!firebase.auth().currentUser) { // if the user's not logged in, handle login
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("success");
+    }).catch(function(error) {
+      console.error("error", error);
+    });
+  } else { // handle logout
+    firebase.auth().signOut();
+  }
+  //This disables the button until login or logout is successful. You'll want to replace 'login-button' with the id of your login button.
+  $('#login-button').attr("disabled", true);
+}
